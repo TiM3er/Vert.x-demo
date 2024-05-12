3819564cdfddd3e245c4593077ed21d70597c4de
Piotr "TiM3er" Witowski <witowskipiotr11@gmail.com>
2024-05-12 16:45:40 +0200

    Feat: add route

--- src/main/java/pl/tim3erland/demo/MainVerticle.java
+++ src/main/java/pl/tim3erland/demo/MainVerticle.java
@@ -1,16 +1,22 @@
 package pl.tim3erland.demo;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
-        vertx.createHttpServer()
-        .requestHandler(req ->
-        {
-            req.response().end("Heeelo Vert.x World!");
-        }).listen(8080);
+        var router = Router.router(vertx);
+        router.get("/api/v1/hello").handler(routingContext -> {
+            routingContext.request().response().end("Hello Vert.x World!");
+        });
+        router.get("/api/v1/hello/:name").handler(routingContext -> {
+            var name = routingContext.pathParam("name");
+            routingContext.request().response().end(String.format("Hello " + name + " !")  );
+        });
+
+        vertx.createHttpServer().requestHandler(router).listen(8080);
     }
 }
 //
